75e19a8cb4a7d88e35033f41723b90c1983677dc
kassifritz <kassianafritz@gmail.com>
2021-08-22 20:35:47 -0300

    Texturas finais

---
+++ AstrocatGourmert/Assets/Scripts/UI/WordsManager.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WordsManager : MonoBehaviour
+{
+    [SerializeField] private Animator animator;
+
+    [SerializeField] private GameObject perfect;
+    [SerializeField] private GameObject great;
+    [SerializeField] private GameObject good;
+    [SerializeField] private GameObject bad;
+
+    private Action onFinish;
+
+    private static WordsManager instance;
+
+    private void Awake()
+    {
+        instance = this;
+        perfect.SetActive(false);
+        great.SetActive(false);
+        good.SetActive(false);
+        bad.SetActive(false);
+    }
+
+    public static void EnableGood(Action listener)
+    {
+        instance.onFinish += listener;
+        instance.good.SetActive(true);
+        instance.animator.SetTrigger("animate");
+    }
+
+    public void OnFinishGoodOutro()
+    {
+        onFinish?.Invoke();
+    }
+}
